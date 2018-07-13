589a09228a43f2d4bb28f57e6259c16bcdfc719d
oqmlkkl <z27fang@Lenovo-PC.localdomain>
2018-07-12 22:51:45 -0400

    gradient descent now updates theta0

--- gradientDescent/gradientDescent.py
+++ gradientDescent/gradientDescent.py
@@ -1,22 +1,36 @@
 import numpy as np
 
-def computeCost(X, y, theta):
+def computeCost(X, y, theta, m):
     resultMatrix = np.multiply(X, theta) - y
-    return np.multiply(resultMatrix, resultMatrix).sum / (2 * m)
+    return np.multiply(resultMatrix, resultMatrix).sum() / (2 * m)
 
 #theta should be a row matrix
 def gradientDescent(X, y, theta, alpha, num_iters):
     #length of y
-    m = y.shape[1]
+    m = y.shape[0]
+
+    const = np.ones((m, 1))
+    X = np.append(const, X, axis = 1)
+    print('X = ', X)
+    #only intialize the const
+    theta = np.insert(theta, 1,1,axis = 0)
+    print('theta = ', theta)
 
     #use J_history to mark the cost
     J_history = 0
 
-    for i in range(1: num_iters):
+    for i in range(0, num_iters):
         thetaOld = theta
         # here using a linear model for now
-        diff = np.multiply(theta, X) - y
-        theta = theta - alpha * diff.sum() / m
-        J_history = computeCost(X, y, theta)
+        hypothesis = np.multiply(theta, X)
+        diff = hypothesis - y
+        print('theta = ', theta)
 
-    return (theta, J_history)
+        #update theta0
+        gradient = alpha * diff.sum() / m
+        theta = theta - np.insert(np.zeros(y.shape[1]), 0, gradient, axis = 0)
+        #update the rest theta
+        #sleepy leave it tomorrow
+        J_history = computeCost(X, y, theta, m)
+
+    return theta
