ff81749d2fab8c721e51f5c56989532a4ad2b292
oqmlkkl <xhfangzihao@gmail.com>
2018-07-25 22:50:11 -0400

    Code updated using high level np

--- LogisticRegression/main.py
+++ LogisticRegression/main.py
@@ -1,5 +1,13 @@
 import datagen as g
+from sigmoid import LogisticRegression
 
 sampleArr = g.gen(1000)
 x1 = sampleArr[0]
 x2 = sampleArr[1]
+
+print('x1 = ', x1)
+print('x2 = ', x2)
+# combine x1 x2 as a new X, and mark them as 0, 1
+
+sig = LogisticRegression(0.005, 100)
+
--- LogisticRegression/sigmoid.py
+++ LogisticRegression/sigmoid.py
@@ -1,13 +1,11 @@
 import numpy as np
 from scipy.optimize import fmin
 
-class LogisticRegression:
+class LogisticRegression(object):
 
-    alpha = 0.005
-    #hTheta = 0
-
-    def __init__(self, alpha):
+    def __init__(self, alpha=0.05, num_iters=100):
         self.alpha = alpha
+        self.num_iters = num_iters
 
     #Why we need sigmoid funtion:
     # In classification problems(start as binary), we need to
@@ -15,27 +13,41 @@ class LogisticRegression:
     # here sigmoid funtion gives us a interval between 0
     # and 1 and distributed evenly between 0 and 1
     def sigmoid(z):
-        return 1/(1 + math.exp(-z))
+        return 1/(1 + np.exp(-z))
+
+    def getH(theta, X):
+        return self.sigmoid(np.dot(X, theta))
+
 
     def setHTheta(self, theta, X, y):
         self.hTheta = self.sigmoid(np.dot(X, theta))
 
     #Define the gradient of cost function of logistic regression.
-    def getGrad(self, theta, X, y):
+    def getGrad(self,theta, X, y):
         #hTheta is the result of hypothesis function with given theta
-        hTheta = np.apply_along_axis(self.sigmoid, 1, np.dot(X, theta))
+''' hTheta = np.apply_along_axis(self.sigmoid, 1, np.dot(X, theta))
         diff = hTheta - y
         gradient = np.dot(np.transpose(X), diff).sum() * self.alpha / X.shape[0]
-        return gradient
+        return gradient'''
+        return np.dot(X.T, self.getH(theta, X)) / y.shape[0]
 
     #Define the cost function
     def getCost(self, theta, X, y):
-        hTheta = np.apply_along_axis(self.sigmoid, 1, np.dot(X, theta))
-        loghTheta = np.apply_along_axis(math.log, hTheta)
-        cost = - np.dot(np.transpose(y), loghTheta)[0][0]
-        I = np.asmatrix(np.ones(y.shape(0)))
-        cost -= np.dot((I - y.transpose()),
-                        np.apply_along_axis(math.log, (I.transpose() - hTheta)))[0][0]
-        return cost/y.shape(0)
+        h = self.getH(theta, X)
+        logH = np.log(h)
+        logOne_H = np.log(1 - h)
+        return (-y * logH - (1 - y) * logOne_H).mean()
 
+    #0.5 as default, may vary along dataset
+    def predict(self, X, theta, threshold=0.5):
+        value = self.sigmoid(np.dot(X, theta))
+        return value >= threshold
+
+
+    def fit(self, X, y):
+        self.theta = np.zeros(X.shape[1])
+        for i in range(self.num_iters):
+            gradient = self.getGrad(self.theta, X, y)
+            self.theta -= self.alpha * gradient
+            print('loss = ', self.getCost(self.theta, X, y))
 
