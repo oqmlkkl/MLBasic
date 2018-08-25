e7bff5123dd1980c2bfcf95afa2e6bab7254c829
oqmlkkl <xhfangzihao@gmail.com>
2018-08-25 15:41:28 -0400

    typo fixed

--- simpleNN/loadData.py
+++ simpleNN/loadData.py
@@ -1,6 +1,7 @@
 import cPickle, gzip, numpy
 
 #load mnist dataset from my own mnist directory
-f = gzip.open('./../../../dataset/mnist/mnist.pl.gz', 'rb')
+f = gzip.open('./../../../dataset/mnist/mnist.pkl.gz', 'rb')
 train_set, valid_set, test_set = cPickle.load(f)
+# print(train_set[0][0].shape)
 f.close()
