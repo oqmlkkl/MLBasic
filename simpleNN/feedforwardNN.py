b94370a0246009e82e125983ceaa3799798f514e
oqmlkkl <xhfangzihao@gmail.com>
2018-08-29 18:11:15 -0400

    correct the logic of creating theta

--- simpleNN/feedforwardNN.py
+++ simpleNN/feedforwardNN.py
@@ -18,11 +18,15 @@ class feedforwardNN:
             print(error)
 
     #randomly initialize theta based on symmetry breaking between range:[-epsion, epsilon]
-    def random_init(self, epsilon):
+    def random_init(self, epsilon, X, y):
         self.theta = ()
         for i in range(0, self.num_hidden_layers):
-            cur_theta = np.full((1, self.neurons[i] +1), random.uniform(-epsilon, epsilon))
-            self.theta += (cur_theta)
+            if i == 0:
+                self.theta += numpy.random.rand(X.shape()[1] + 1, self.neurons[0])
+            else if i == self.num_hidden_layers - 1:
+                self.theta += numpy.random.rand(self.neurons[i] + 1, y.shape()[1])
+            else:
+                self.thata += numpy.random.rand(self.neurons[i] + 1, self.neurons[i + 1])
         print('theta = ' + self.theta)
 
     def setData(self, training_set, validation_set, test_set):
@@ -33,7 +37,7 @@ class feedforwardNN:
     def sigmoid(self, z):
         return 1/(1 + np.exp(-z))
 
-    def activation(self, X, theta):
+    def get_activations(self, X, theta):
         z = X * theta.T
         return np.concatenate(np.ones(z.shape[0]).T, self.sigmoid(z))
 
@@ -43,12 +47,20 @@ class feedforwardNN:
             delta += (np.full((1, self.neurons[i]), 0))
         return delta
 
-    # implement the back propagation algorithm
-    def back_propagation(self):
-        # step1: initialize the delta matrix
-        delta_matrix = self.get_init_delta()
-        # step2: do the forward propagation
-
+    # implement the back propagation algorithm once, the delta_matrix is a passed in parameter
+    def back_propagation(self, X, y, delta_matrix):
+        # step1: do the forward propagation to get the delta of output,
+        #        and store the activations for back propagation
+        activations = ()
+        for i in range(0, self.num_hidden_layers + 1):
+            if i == 0:
+                activations += (np.concatenate(np.ones(1), X))
+            else:
+                activations += (np.ones(self.neurons[i]))
+        #forward propagation, update activations
+        for i in range(1, self.num_hidden_layers):
+            activations[i] = self.get_activation(activations[i - 1], self.theta[i - 1])
+        # setp2: update delta_matrix
 
 
     def cost():
