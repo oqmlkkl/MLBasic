52ca2118cef81ecf9a9b689bfc8424df4012c455
oqmlkkl <xhfangzihao@gmail.com>
2018-09-05 20:44:58 -0400

    learning rate updated

--- simpleNN/main.py
+++ simpleNN/main.py
@@ -3,6 +3,6 @@ from loadData import data_set
 import pdb
 data_set = data_set()
 neurons = 30
-network = feedforwardNN(1, neurons, data_set.get_trainset(), 30)
+network = feedforwardNN(1, neurons, data_set.get_trainset(), 0.05)
 print(data_set.get_trainset()[0][0].shape)
 network.stochastic_gradient_descent(30, 30, data_set.get_testset())
