1ce1eb3e6b1f540c35a71757db35472871649597
oqmlkkl <xhfangzihao@gmail.com>
2018-09-08 12:19:29 -0400

    update main function

--- simpleNN/main.py
+++ simpleNN/main.py
@@ -1,8 +1,15 @@
 from feedforwardNN import feedforwardNN
 from loadData import data_set
 import pdb
+
+#define parameters
 data_set = data_set()
+num_hidden_layer = 1
 neurons = 30
-network = feedforwardNN(1, neurons, data_set.get_trainset(), 0.05)
-print(data_set.get_trainset()[0][0].shape)
-network.stochastic_gradient_descent(50, 30, data_set.get_testset())
+learning_rate = 0.05
+batch_size = 30
+epoch = 50
+
+#start training
+network = feedforwardNN(num_hidden_layer, neurons, data_set.get_trainset(), learning_rate)
+network.stochastic_gradient_descent(epoch, batch_size, data_set.get_testset())
