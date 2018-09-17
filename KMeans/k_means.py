a5ae5ccc71d536c45b47fddd211cad317eebfeb0
oqmlkkl <xhfangzihao@gmail.com>
2018-09-17 15:50:43 -0400

    k means implemented

--- KMeans/k_means.py
+++ KMeans/k_means.py
@@ -1,19 +1,24 @@
 import numpy as np
-
+import math
 def euclid_distance(X1, X2):
-    return np.linalg.norm(X1, X2)
+    #return np.linalg.norm(X1, X2)
+    sqrsum = 0
+    for i in range(len(X1)):
+        sqrsum += (X1[i] - X2[i]) ** 2
+    return math.sqrt(sqrsum)
 
 class k_means():
     """K defines the number of clusters that the algorithm needs to find.
        X is the training X, and y is the training y,
        num_iters is the number of iterations that needs to perform,
        is_noisy is a boolean to control the stdout"""
-    def __init__(self, k, X, y, num_iters, is_noisy):
+    def __init__(self, k, X, y, num_iters, is_noisy, tolerance):
         self.is_noisy = is_noisy
         self.random_initialize(k, X)
         self.num_iters = num_iters
-        # Convert each X into a tuple where the second element is the class
-        self.X = [(x, 0) for x in X]
+        self.clusters = [ []  for i in range(k)]
+        self.X = X
+        self.tolerance = tolerance
         if(is_noisy):
             print('Init completed.')
 
@@ -29,13 +34,44 @@ class k_means():
 
     def classify(self):
         for i in range(self.num_iters):
+            self.clusters = [[] for i in range(0, len(self.centroids))]
             for x in self.X:
-                distances = [ euclid_distance(x[0], centroid) in self.centroids ]
-                x[1] = distances.index(min(distances))
+                distances = [ euclid_distance(x, centroid) for centroid in self.centroids ]
+                self.clusters[distances.index(min(distances))].append(x)
+            old_centroids = self.centroids
             self.update_centroids()
             if(self.is_noisy):
-                print "> epoch {0} completed, new centroids: {1}".format(i, self.centroids)
+                print("> epoch {0} completed, new centroids: {1}".format(i, self.centroids_toString()))
+            if(self.tolerance!=0):
+                if(self.is_noisy):
+                    print("Checking tolerance ...")
+                if(self.check_tolerance(old_centroids)):
+                    break
 
-    def udpate_centroids(self):
-        #TODO
+    def centroids_toString(self):
+        string_centroids = []
+        for centroid in self.centroids:
+            string_centroids.append(np.array2string(centroid, precision=2, separator=',', suppress_small=True))
+        return ','.join(string_centroids)
 
+    def train(self):
+        self.classify()
+        resultX = []
+        resultLabel = []
+        for i in range(0,len(self.clusters)):
+            for item in self.clusters[i]:
+                resultX.append(item)
+                resultLabel.append(i)
+        return (resultX, resultLabel)
+
+
+    def update_centroids(self):
+        for i in range(len(self.clusters)):
+            cur_cluster = self.clusters[i]
+            self.centroids[i] = np.average(cur_cluster, axis = 0)
+
+    def check_tolerance(self, old_centroids):
+        isOptimal = False
+        for i in range(len(self.centroids)):
+            isOptimal = isOptimal and np.sum((old_centroids[i] - self.centroids[i]) / self.centroids[i] * 100) < tolerance
+        return isOptimal
--- KMeans/load_iris_data.py
+++ KMeans/load_iris_data.py
@@ -14,4 +14,11 @@ X = iris.data[:, :2]
 y = iris.target
 plt.scatter(X[:,0], X[:,1])
 plt.show()
-classifier = k_means(3, X, y, 300, True)
+classifier = k_means(3, X, y, 300, True, 0.1)
+result = classifier.train()
+
+trained_x = [ x.tolist() for x in result[0] ]
+label = result[1]
+plt.scatter([x[0] for x in trained_x],
+            [x[1] for x in trained_x], c=label, s=50, alpha=0.75)
+plt.show()
