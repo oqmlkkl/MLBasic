0b640bf4d73e668af547ebbe12b1679416ff5538
z27fang <z27fang@outlook.com>
2019-02-06 22:27:20 -0500

    init commit for svm

--- Unsupervised/svm.py
+++ Unsupervised/svm.py
@@ -1,8 +1,15 @@
 from sklearn.datasets import load_breast_cancer
 from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import MinMaxScaler
+
 cancer = load_breast_cancer()
 
 X_train, X_test, Y_train, Y_test = train_test_split(cancer.data, cancer.target, random_state=1)
 
 print(X_train.shape)
 print(Y_train.shape)
+
+#Use scalar to fit the X_train
+
+scaler.fit(X_train)
+MinMaxScaler(copy=True, feature_range=(0, 1))
